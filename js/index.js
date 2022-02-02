4780b841b7c7ec409d99f09bff29aed8b6d7d53b
Eronice Moura <evdm.moura@gmail.com>
2022-02-02 10:54:10 -0400

    Arquivo JavaScript adicionado

---
+++ js/index.js
@@ -0,0 +1,13 @@
+//console.log("Fui carregado de um novo aruqivo externo");
+//var titulo = document.querySelector(".titulo");
+//titulo.textContent = "Aparecida Nutricionista";
+
+var botaoCript = document.querySelector("#bt-criptografar");
+    botaoCript.addEventListener("click", function(){
+        
+        var textoUsuario = document.querySelector("#texto-cript");
+
+        textoUsuario
+        console.log(textoUsuario.value);
+
+    })
